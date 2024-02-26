332bb822108532d9e2e3e947d01a1d23c0280c86
Harsh Cheema <98507707+harsh-cheema@users.noreply.github.com>
2024-02-26 21:17:30 +0530

    Update Student.java

--- src/main/java/com/example/student/model/Student.java
+++ src/main/java/com/example/student/model/Student.java
@@ -1,12 +1,6 @@
 package com.example.student.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
 public class Student {
     public String id;
 
